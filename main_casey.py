889a246de64415822d2014047f200ee04f9bf4fe
Sake <cnort318@evit.edu>
2025-09-25 13:04:32 -0700

    fdsa

--- main_casey.py
+++ main_casey.py
@@ -711,7 +711,7 @@ while run:
     draw_health_masks(player.current_masks, player.max_masks)
     
     # Check if player died and restart game
-    if not player.alive:
+    if not player.alive or not enemy.alive:
         restart_game()
 
     for event in pygame.event.get():
